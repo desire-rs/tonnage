b23b183ab6dd042928a94cf163b51ce0d8ea3c63
Mankong zhao <mankong.zhao@aax.com>
2022-11-01 00:22:34 +0800

    fix about

--- web/About.vue
+++ web/About.vue
@@ -1,34 +1,15 @@
 <template lang="pug">
 .container
   .form
-    video(src="videos/mov_bbb.mp4" controls)
+    video(src="videos/mov_bbb.mp4" controls style="width: 600px")
 </template>
-
-  <script>
+<script lang="ts">
 import axios from "axios";
 axios.defaults.baseURL = window.location.href.split("#")[0];
 export default {
   data() {
-    return {
-      username: "",
-      password: "",
-    };
+    return {};
   },
-  methods: {
-    async submit() {
-      if (!this.username || !this.password) {
-        alert("username or password can not empty");
-      }
-      const data = { username: this.username, password: this.password };
-      const result = await axios.post("/signin", data);
-      const { token, exp } = result.data.data;
-      if (token) {
-        window.localStorage.setItem("token", token);
-        window.localStorage.setItem("exp", exp);
-        window.localStorage.setItem("username", this.username);
-      }
-      this.$router.push("/");
-    },
-  },
+  methods: {},
 };
 </script>
